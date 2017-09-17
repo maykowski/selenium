32ceae15129380b78b9a11f51511afd73d1367f1
maykowski <wojtek.maykowski@gmail.com>
2017-09-17 13:42:46 +0200

    init

---
+++ src/test/java/com/eviltester/webdriver/MyFirstTest.java
@@ -0,0 +1,17 @@
+package com.eviltester.webdriver;
+
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyFirstTest {
+    @Test
+    public void startWebDriver(){
+        System.setProperty("webdriver.gecko.driver","gecko/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://onet.pl");
+        driver.findElement(By.cssSelector("li.mail a")).click();
+    }
+}
