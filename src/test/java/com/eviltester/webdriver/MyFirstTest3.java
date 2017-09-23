4df1ecfbd10c78c731f7c081f1d93676b4cf4774
MarcinMaykowski <marcin.maykowski@gmail.com>
2017-09-23 13:33:50 +0200

    MOj 4ci test

---
+++ src/test/java/com/eviltester/webdriver/MyFirstTest3.java
@@ -0,0 +1,23 @@
+package com.eviltester.webdriver;
+
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyFirstTest3 {
+    @Test
+    public void startWebDriver(){
+        System.setProperty("webdriver.gecko.driver","gecko/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        //Path sampleFile = Paths.get("C:\\Users\\User\\Desktop\\HTML\\body.samolot.html");
+        driver.get("http://localhost:8080/body.samolot.html");
+        //driver.get("file:///C:/Users/User/Desktop/HTML/body.samolot.html");
+        Assert.assertEquals("samolot",driver.findElement(By.cssSelector(".samolot")).getText());
+        //driver.findElement(By.cssSelector(".samolot")).getText();
+        driver.close();
+    }
+}
